0c5c272c8d774bc74105f6fc971d8c7a912db770
dorukyurdusen <dorukyurdusen@gmail.com>
2022-08-12 15:15:15 +0300

    asd

--- src/sentimentAnalysis.py
+++ src/sentimentAnalysis.py
@@ -1,17 +1,1 @@
-#implement sentiment analysis
-import nltk
-import re
-import random
-from nltk.corpus import movie_reviews
-from nltk.classify.scikitlearn import SklearnClassifier
-import pickle
-from sklearn.naive_bayes import MultinomialNB, BernoulliNB
-from sklearn.linear_model import LogisticRegression, SGDClassifier
-from sklearn.svm import SVC, LinearSVC, NuSVC
-from nltk.classify import ClassifierI
-from statistics import mode
-from nltk.tokenize import word_tokenize
-fdghgdhgdhgfdh
-hghghgfdhgfdhgfhgdhgds
-sadasfsadfasd
-asdfasdfasdfasdfasfd
+abc
