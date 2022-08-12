ee0776813d98a008c4f0ff077103a639ddddaeb0
Yiğit <yigit.kilicarslan@outlook.com>
2022-08-12 18:48:55 +0300

    a

--- src/sentimentAnalysis.py
+++ src/sentimentAnalysis.py
@@ -1,4 +1,4 @@
-#pip install vaderSentiment
+# pip install vaderSentiment
 
 # import SentimentIntensityAnalyzer class
 # from vaderSentiment.vaderSentiment module.
@@ -6,17 +6,18 @@ from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
 # function to print sentiments
 # of the sentence.
-def sentiment_scores(sentence):
-	sid_obj = SentimentIntensityAnalyzer()
-	sentiment_dict = sid_obj.polarity_scores(sentence)
-	print("Sentence:",sentence)
-	print("Sentiment score is ", sentiment_dict['compound'])
-	print("Overall sentiment dictionary is : ", sentiment_dict)
-    
 
+
+def sentiment_scores(sentence):
+    sid_obj = SentimentIntensityAnalyzer()
+    sentiment_dict = sid_obj.polarity_scores(sentence)
+    print("")
+    print("Sentence:", sentence)
+    print("Sentiment score is ", sentiment_dict['compound'])
+    print("")
 
 
 # Driver code
-if __name__ == "__main__" :
-	sentiment_scores("study is going on as usual")
-	sentiment_scores("I am very sad today.")
+if __name__ == "__main__":
+    sentiment_scores("study is going on as usual")
+    sentiment_scores("I am very sad today.")
